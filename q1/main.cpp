4e4cb30cb94f8e3117fe00ab81acdc2e0aafa4b9
Mike Chaberski <mchaberski@gmail.com>
2020-02-21 22:33:09 -0500

    q1 implemented with args instead of stdin

--- q1/main.cpp
+++ q1/main.cpp
@@ -10,13 +10,25 @@ using namespace std;
 
 const char SEPARATOR = '\t';
 const int DAYS_PER_WEEK = 7;
+const int M_JAN = 1;
+const int M_FEB = 2;
+const int M_MAR = 3;
+const int M_APR = 4;
+const int M_MAY = 5;
+const int M_JUN = 6;
+const int M_JUL = 7;
+const int M_AUG = 8;
+const int M_SEP = 9;
+const int M_OCT = 10;
+const int M_NOV = 11;
+const int M_DEC = 12;
 
 int printMonthCalendar(int numDays, int startingDay) {
     assert(startingDay >= 1 && startingDay <= DAYS_PER_WEEK);
     cout << "Mon" << SEPARATOR 
          << "Tue" << SEPARATOR 
          << "Wed" << SEPARATOR 
-         << "Thu" << SEPARATOR 
+         << "Thu" << SEPARATOR    // question has this as "Thr" 
          << "Fri" << SEPARATOR 
          << "Sat" << SEPARATOR 
          << "Sun" << endl;
@@ -38,6 +50,97 @@ int printMonthCalendar(int numDays, int startingDay) {
     return lastDay;
 }
 
+bool isLeapYear(int year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
+int getNumDaysInMonth(int month, int year) {
+    switch (month) {
+        case M_FEB:
+            if (isLeapYear(year)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        case M_JAN:
+        case M_MAR:
+        case M_MAY:
+        case M_JUL:
+        case M_AUG:
+        case M_OCT:
+        case M_DEC:
+            return 31;
+        case M_APR:
+        case M_JUN:
+        case M_SEP:
+        case M_NOV:
+            return 30;
+        default:
+            cerr << "getNumDaysInMonth: invalid month: " << month << endl;
+            assert(false);
+            return 0;
+    }
+}
+
+void printMonthName(int month) {
+    switch (month) {
+        case M_JAN:
+            cout << "January";
+            break;
+        case M_FEB:
+            cout << "February";
+            break;
+        case M_MAR:
+            cout << "March";
+            break;
+        case M_APR:
+            cout << "April";
+            break;
+        case M_MAY:
+            cout << "May";
+            break;
+        case M_JUN:
+            cout << "June";
+            break;
+        case M_JUL:
+            cout << "July";
+            break;
+        case M_AUG:
+            cout << "August";
+            break;
+        case M_SEP:
+            cout << "September";
+            break;
+        case M_OCT:
+            cout << "October";
+            break;
+        case M_NOV:
+            cout << "November";
+            break;
+        case M_DEC:
+            cout << "December";
+            break;
+        default:
+            cerr << "printMonthName: invalid month: " << month << endl;
+            assert(false);
+    }
+}
+
+void printYearCalendar(int year, int startingDay) {
+    for (int month = M_JAN; month <= M_DEC; month++) {
+        if (month > M_JAN) {
+            cout << endl;
+        }
+        printMonthName(month);
+        cout << ' ' << year << endl;
+        int numDays = getNumDaysInMonth(month, year);
+        int lastDay = printMonthCalendar(numDays, startingDay);
+        // +1 to advance a day, -1 to map [1.7] range to [0,6] range for mod, 
+        // and then another +1 to map back to [1,7] range
+        startingDay = ((lastDay + 1 - 1) % DAYS_PER_WEEK) + 1;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         cerr << "usage error: two arguments required" << endl;
@@ -48,8 +151,6 @@ int main(int argc, char* argv[]) {
 //    cin >> calendarYear >> calendarStartingDay;
     calendarYear = (int) strtol(argv[1], nullptr, 10);
     calendarStartingDay = (int) strtol(argv[2], nullptr, 10);
-    printMonthCalendar(calendarYear, calendarStartingDay);
-        
-
+    printYearCalendar(calendarYear, calendarStartingDay);
     return 0;
 }
