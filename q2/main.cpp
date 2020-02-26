bf5d25516b6629c8549b79a4a1efdeeb0f322446
Mike Chaberski <mac937@nyu.edu>
2020-02-25 19:06:22 -0500

    q2: add comments

--- q2/main.cpp
+++ q2/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 // Analyzes the divisors of num, assigning the number of proper divisors of
 // num to outCountDivs and the sum of num's proper divisors to outSumDivs.
+// Iterates up to sqrt(num) times, so the algorithm is O(sqrt(num)).
 void analyzeDivisors(int num, int& outCountDivs, int& outSumDivs) {
     double numSqrt = sqrt(num);
     outCountDivs = 1;  // 1 is a divisor of all integers
@@ -27,12 +28,24 @@ void analyzeDivisors(int num, int& outCountDivs, int& outSumDivs) {
     }
 }
 
+// Checks whether a number is a perfect number.
+// 
+// This is defined here because Q2 part (b) says to implement it, but we don't 
+// use it in the program because for each iteration we do additional work with 
+// the proper divisor sum, and if we called this function to check 
+// perfect-ness, then we would have to call analyzeDivisors() an additional 
+// time.
 bool isPerfect(int num) {
     int properDivisorSum, properDivisorCount;
     analyzeDivisors(num, properDivisorCount, properDivisorSum);
     return num == properDivisorSum;
 }
 
+// Prompts user for an integer M and prints perfect and amicable numbers 
+// in the range [2, M].
+// 
+// Iterates from 2 to M and invokes analyzeDivisors at most twice per 
+// iteration, so the algorithm is O(M^1.5).
 int main() {
     int maxNum;
     int numDivisorCount, numDivisorSum;
